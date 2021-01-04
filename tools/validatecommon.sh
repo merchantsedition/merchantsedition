9c8de92e370b99f32fd4ec1a725a268dcaf8450c
Markus Hitter <mah@merchantsedition.com>
2021-01-04 19:28:06 +0100

    validatecore.sh: ensure absence of old documentation files.
    Again, implemented by moving relevant code from validatemodule.sh
    to validatecommon.sh.

--- tools/validatecommon.sh
+++ tools/validatecommon.sh
@@ -363,4 +363,26 @@ function validate_documentation {
     # Wrong capitalization.
     e "file ${README} exists, but should be named 'README.md' (capitalization)."
   fi
+
+  # Former documentation files should be absent.
+  FILES=('readme')
+  FILES+=('readme.txt')
+  FILES+=('roadmap')
+  FILES+=('roadmap.md')
+  FILES+=('roadmap.txt')
+  FILES+=('contributing')
+  FILES+=('contributing.md')
+  FILES+=('contributing.txt')
+
+  FAULT='false'
+  for F in "${FILES[@]}"; do
+    UNWANTED=$(${FIND} . | grep -i '^'"${F}"'$')
+    if [ -n "${UNWANTED}" ]; then
+      e "file ${UNWANTED} shouldn't exist."
+      FAULT='true'
+    fi
+  done
+  [ ${FAULT} = 'true' ] && \
+    n "content of such former documentation files goes into README.md now."
+  unset FILES FAULT UNWANTED
 }
--- tools/validatemodule.sh
+++ tools/validatemodule.sh
@@ -355,28 +355,6 @@ fi
 
 validate_documentation
 
-# Former documentation files should be absent.
-FILES=('readme')
-FILES+=('readme.txt')
-FILES+=('roadmap')
-FILES+=('roadmap.md')
-FILES+=('roadmap.txt')
-FILES+=('contributing')
-FILES+=('contributing.md')
-FILES+=('contributing.txt')
-
-FAULT='false'
-for F in "${FILES[@]}"; do
-  UNWANTED=$(${FIND} . | grep -i '^'"${F}"'$')
-  if [ -n "${UNWANTED}" ]; then
-    e "file ${UNWANTED} shouldn't exist."
-    FAULT='true'
-  fi
-done
-[ ${FAULT} = 'true' ] && \
-  n "content of such former documentation files goes into README.md now."
-unset FILES FAULT UNWANTED
-
 if ${FIND} README.md | grep -q '.'; then
   # These are needed as delimiters, so check for their presence early.
   HEADINGS=('Description')
