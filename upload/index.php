8506e04715ea48f8bfc2673019dec524d1e22c7b
Markus Hitter <mah@merchantsedition.com>
2021-01-08 01:03:03 +0100

    Matching index.php files.
    Remaining 49 files found by validatecore.sh. One file in tests/
    removed, because tests don't end up on the server.

--- upload/index.php
+++ upload/index.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Copyright (C) 2021 Merchant's Edition GbR
  * Copyright (C) 2020 thirty bees
  *
  * NOTICE OF LICENSE
@@ -10,10 +11,12 @@
  * https://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@thirtybees.com so we can send you a copy immediately.
+ * to contact@merchantsedition.com so we can send you a copy immediately.
  *
+ * @author    Merchant's Edition <contact@merchantsedition.com>
  * @author    thirty bees <contact@thirtybees.com>
- * @copyright 2019 thirty bees
+ * @copyright 2021 Merchant's Edition GbR
+ * @copyright 2020 thirty bees
  * @license   Open Software License (OSL 3.0)
  */
 
