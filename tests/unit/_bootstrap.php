b9bb02ae22cb934a972e49e32ee372237433701c
Markus Hitter <mah@merchantsedition.com>
2021-01-11 13:27:38 +0100

    100 matching PHP headers.
    Found by validatecore.sh. Just 100 files, to make the commit not
    too big.
    
    Some files had no header at all, they were added according to what
    they should have been.
    
    Surprisingly enough, this commit changes only 100 files, but the
    number of not yet matching PHP files was reduced by 101 files,
    from 624 to 523.

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * Copyright (C) 2021 Merchant's Edition GbR
+ * Copyright (C) 2017-2018 thirty bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@merchantsedition.com so we can send you a copy immediately.
+ *
+ * @author    Merchant's Edition <contact@merchantsedition.com>
+ * @author    thirty bees <contact@thirtybees.com>
+ * @copyright 2021 Merchant's Edition GbR
+ * @copyright 2017-2018 thirty bees
+ * @license   Open Software License (OSL 3.0)
+ */
+
 // Here you can initialize variables that will be available to your tests
 require_once __DIR__.'/../../vendor/autoload.php';
 
--- tools/generatemd5list.php
+++ tools/generatemd5list.php
@@ -1,28 +1,23 @@
 <?php
 /**
+ * Copyright (C) 2021 Merchant's Edition GbR
  * Copyright (C) 2017-2018 thirty bees
- *
- * thirty bees is an extension to the PrestaShop e-commerce software developed by PrestaShop SA
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * https://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@thirtybees.com so we can send you a copy immediately.
+ * to contact@merchantsedition.com so we can send you a copy immediately.
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade thirty bees to newer
- * versions in the future. If you want to customize thirty bees for your
- * needs please refer to https://www.thirtybees.com for more information.
- *
- *  @author    thirty bees <contact@thirtybees.com>
- *  @copyright 2017-2018 thirty bees
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author    Merchant's Edition <contact@merchantsedition.com>
+ * @author    thirty bees <contact@thirtybees.com>
+ * @copyright 2021 Merchant's Edition GbR
+ * @copyright 2017-2018 thirty bees
+ * @license   Open Software License (OSL 3.0)
  */
 
 // These should work with or without installation.
