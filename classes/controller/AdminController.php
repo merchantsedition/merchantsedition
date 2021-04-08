f4c45f605613a6d1ae94ca8d00f7ee879e772428
Petr Hučík <petr@getdatakick.com>
2021-04-08 11:52:07 +0200

    Remove Currency Auto Format from core
    Currency auto formatting functionality was removed from core, and
    replaced with standard extension point using hook functionality.
    
    This commit introduces new hook actionGetCurrencyFormatters. Modules can
    use this hook to provide custom currency formatters, both for php and
    js. Core code no longer need third party library commerceguys/intl.
    
    Original code was extracted to new module thirtybees/tbcurrencyformatter.
    By installing this module, merchants can continue to use auto formatting
    functionality. Because third party commerceguys/intl needs php 70, this
    module can be safely used only on that platform.
    
    Closes #1314
    Closes #1157
    
    Changes by Traumflug:
    
     - Edited composer.json rather than composer/php5.6/composer.json, etc.
    
     - Updated composer.lock accordingly.

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -3823,8 +3823,7 @@ class AdminControllerCore extends Controller
         $this->addJqueryUI(['ui.slider', 'ui.datepicker']);
 
         Media::addJsDef(['host_mode' => false]);
-        // @since 1.0.2
-        Media::addJsDef(['currencyModes' => Currency::getModes()]);
+        Media::addJsDef(['currencyFormatters' => Currency::getJavascriptFormatters()]);
 
         $this->addJS(
             [
